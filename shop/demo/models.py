ac90830be48118aad0213f4d57e6376699e29002
guv <jurapro@yandex.ru>
2023-10-24 14:26:02 +0700

    модель категории

--- shop/demo/models.py
+++ shop/demo/models.py
@@ -45,3 +45,10 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254, verbose_name='Наименование', blank=False)
+
+    def __str__(self):
+        return self.name
