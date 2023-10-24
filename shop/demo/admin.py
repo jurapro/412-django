6d9fc07bab0162f7948b9dcbef0e398fd58d6c20
guv <jurapro@yandex.ru>
2023-10-24 12:28:57 +0700

    добавили админку

--- shop/demo/admin.py
+++ shop/demo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from demo.models import *
+
+
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Order)
